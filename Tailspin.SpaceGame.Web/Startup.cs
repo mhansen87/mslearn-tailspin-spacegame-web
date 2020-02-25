88cf9b9c2c000bdd250d5299388e83e3954d0bfd
Mikkel Hansen (Valtech) <mikkel.hansen@valtech.com>
2020-02-25 12:02:56 +0100

    Resolve conflicts

--- Tailspin.SpaceGame.Web/Startup.cs
+++ Tailspin.SpaceGame.Web/Startup.cs
@@ -7,18 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-<<<<<<< HEAD
-using Microsoft.Extensions.Hosting;
-using TailSpin.SpaceGame.Web.Models;
-using Microsoft.AspNetCore.Http;
-
-
-=======
 using TailSpin.SpaceGame.Web.Models;
 using Microsoft.Extensions.Hosting;
 using Microsoft.AspNetCore.Http;
 
->>>>>>> master
 namespace TailSpin.SpaceGame.Web
 {
     public class Startup
@@ -61,15 +53,7 @@ namespace TailSpin.SpaceGame.Web
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-<<<<<<< HEAD
 
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-=======
 
             app.UseCookiePolicy();
             app.UseRouting();
@@ -77,8 +61,6 @@ namespace TailSpin.SpaceGame.Web
 
             app.UseEndpoints(endpoints =>
              {
-
->>>>>>> master
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
